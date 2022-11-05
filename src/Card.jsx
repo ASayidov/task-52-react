e693b2979127fd4458eabd83fc0d415189d078e4
ASayidov <abdullajonsayidov@gmail.com>
2022-11-05 20:55:43 +0500

    props deleted

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,4 +1,4 @@
-const Card = (props) => {
+const Card = () => {
   return <div className="Card_border">
     <h2>Lorem ipsum dolor sit.</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem enim cum debitis consequatur vel optio nisi earum sit quasi nesciunt magni aliquid eos quidem necessitatibus possimus neque accusantium, exercitationem perspiciatis. Quae soluta deserunt quasi assumenda natus, excepturi illo asperiores exercitationem quidem veniam doloribus accusantium nihil ea ut autem cum? Aut.</p>
