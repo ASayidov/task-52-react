6a906b0005c4cb31023fcd15924d3bc0ab9d527d
ASayidov <abdullajonsayidov@gmail.com>
2022-11-05 20:54:44 +0500

    style changed

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,5 +1,6 @@
 const Card = (props) => {
   return <div className="Card_border">
+    <h2>Lorem ipsum dolor sit.</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem enim cum debitis consequatur vel optio nisi earum sit quasi nesciunt magni aliquid eos quidem necessitatibus possimus neque accusantium, exercitationem perspiciatis. Quae soluta deserunt quasi assumenda natus, excepturi illo asperiores exercitationem quidem veniam doloribus accusantium nihil ea ut autem cum? Aut.</p>
 
   </div>
