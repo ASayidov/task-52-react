8fa95601ab0ede754d679ab7322ba058ff2b3b14
ASayidov <abdullajonsayidov@gmail.com>
2022-11-06 14:04:21 +0500

    optimization cods

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,11 @@
 const Card = (props) => {
 
   const content = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem enim cum debitis consequatur vel optio nisi earum sit quasi nesciunt magni aliquid eos quidem necessitatibus possimus neque accusantium, exercitationem perspiciatis. Quae soluta deserunt quasi assumenda natus, excepturi illo asperiores exercitationem quidem veniam doloribus accusantium nihil ea ut autem cum? Aut."
+  const colorStyle = {color:props.color}
+
   return <div className="Card_border">
-    <h2 style={{color:props.color}}>{props.cardTitle}</h2>
-    <p style={{color:props.color}}>{content}</p>
+    <h2 style={colorStyle}>{props.cardTitle}</h2>
+    <p style={colorStyle}>{content}</p>
 
   </div>
 
