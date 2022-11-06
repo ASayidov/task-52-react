9d1fc1d27a5ce72f0399db4d49537244d89ed6fc
ASayidov <abdullajonsayidov@gmail.com>
2022-11-06 21:27:27 +0500

    changed to destruturing

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,12 +2,13 @@ const Card = (props) => {
 
   const content = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem enim cum debitis consequatur vel optio nisi earum sit quasi nesciunt magni aliquid eos quidem necessitatibus possimus neque accusantium, exercitationem perspiciatis. Quae soluta deserunt quasi assumenda natus, excepturi illo asperiores exercitationem quidem veniam doloribus accusantium nihil ea ut autem cum? Aut."
   const colorStyle = {color:props.color}
+  const {cardTitle, color} = props
 
-  return <div className="Card_border">
-    <h2 style={colorStyle}>{props.cardTitle}</h2>
-    <p style={colorStyle}>{content}</p>
+  return (<div className="Card_border">
+    <h2 style={{color:color}}>{cardTitle}</h2>
+    <p style={{color:color}}>{content}</p>
 
-  </div>
+  </div>)
 
 }
 
