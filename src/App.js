9edb48f059ecf314bb594be25bf9699705be8d5a
ASayidov <abdullajonsayidov@gmail.com>
2022-11-06 20:23:22 +0500

    changed title color

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Card } from "./Card"
 function App() {
 return (
 <div className="App">
- <h1>HTML/CSS Project ideas</h1>
+ <h1 style={{color: "DarkSlateGrey"}}>HTML/CSS Project ideas</h1>
  <Card cardTitle = <i>Card first</i>  color = "red"/>
  <Card cardTitle = <i>Card second</i> color = "blue"/>
  <Card cardTitle = <i>Card third</i>  color = "green"/>
